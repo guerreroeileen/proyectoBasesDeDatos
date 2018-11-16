93a9761e04325f60a52bf89fb6ded7ef7a75c372
Eileen Guerrero <eileenguerrerogomez@gmail.com>
2018-11-16 10:22:47 -0500

    Ventana de inicio de sesion lista

---
+++ src/vista/VentanaInicioSesion.java
@@ -0,0 +1,142 @@
+package vista;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.TitledBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class VentanaInicioSesion {
+
+	private JFrame frmTelefonica;
+	private JTextField textFieldUsuario;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaInicioSesion window = new VentanaInicioSesion();
+					window.frmTelefonica.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public VentanaInicioSesion() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmTelefonica = new JFrame();
+		frmTelefonica.setTitle("Telefonica");
+		frmTelefonica.setBounds(100, 100, 466, 214);
+		frmTelefonica.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "Inicio de sesion", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		frmTelefonica.getContentPane().add(panel, BorderLayout.CENTER);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[] {217, 217, 0};
+		gbl_panel.rowHeights = new int[] {47, 47, 37, 0};
+		gbl_panel.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JLabel lblUsuario = new JLabel("Usuario:");
+		GridBagConstraints gbc_lblUsuario = new GridBagConstraints();
+		gbc_lblUsuario.fill = GridBagConstraints.BOTH;
+		gbc_lblUsuario.insets = new Insets(0, 0, 5, 5);
+		gbc_lblUsuario.gridx = 0;
+		gbc_lblUsuario.gridy = 0;
+		panel.add(lblUsuario, gbc_lblUsuario);
+		
+		textFieldUsuario = new JTextField();
+		GridBagConstraints gbc_textFieldUsuario = new GridBagConstraints();
+		gbc_textFieldUsuario.fill = GridBagConstraints.BOTH;
+		gbc_textFieldUsuario.insets = new Insets(0, 0, 5, 0);
+		gbc_textFieldUsuario.gridx = 1;
+		gbc_textFieldUsuario.gridy = 0;
+		panel.add(textFieldUsuario, gbc_textFieldUsuario);
+		textFieldUsuario.setColumns(10);
+		
+		JLabel lblContrasea = new JLabel("Contrase\u00F1a:");
+		GridBagConstraints gbc_lblContrasea = new GridBagConstraints();
+		gbc_lblContrasea.fill = GridBagConstraints.BOTH;
+		gbc_lblContrasea.insets = new Insets(0, 0, 5, 5);
+		gbc_lblContrasea.gridx = 0;
+		gbc_lblContrasea.gridy = 1;
+		panel.add(lblContrasea, gbc_lblContrasea);
+		
+		passwordField = new JPasswordField();
+		GridBagConstraints gbc_passwordField = new GridBagConstraints();
+		gbc_passwordField.fill = GridBagConstraints.BOTH;
+		gbc_passwordField.insets = new Insets(0, 0, 5, 0);
+		gbc_passwordField.gridx = 1;
+		gbc_passwordField.gridy = 1;
+		panel.add(passwordField, gbc_passwordField);
+		
+		JLabel labelInformacion = new JLabel("");
+		GridBagConstraints gbc_labelInformacion = new GridBagConstraints();
+		gbc_labelInformacion.gridx = 1;
+		gbc_labelInformacion.gridy = 2;
+		panel.add(labelInformacion, gbc_labelInformacion);
+		
+		
+		
+		
+		
+		
+		/**
+		 * Acciones para el boton ingresar
+		 */
+		JButton btnIngresar = new JButton("Ingresar");
+		btnIngresar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				/**
+				 * Se verifica la contraseña y el usuario no sean vacios. Posteiormente se realiza
+				 * la verificacion para ingresar a la base de datos
+				 */
+				if(passwordField.getText().length()>0 && textFieldUsuario.getText().length()>0) {
+					
+				}else {
+					labelInformacion.setText("Usuario y/o contraseña vacios");
+				}
+			}
+		});
+		
+		
+		
+		GridBagConstraints gbc_btnIngresar = new GridBagConstraints();
+		gbc_btnIngresar.fill = GridBagConstraints.BOTH;
+		gbc_btnIngresar.insets = new Insets(0, 0, 0, 5);
+		gbc_btnIngresar.gridx = 0;
+		gbc_btnIngresar.gridy = 2;
+		panel.add(btnIngresar, gbc_btnIngresar);
+		
+	}
+
+}
