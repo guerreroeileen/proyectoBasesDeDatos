67fa02ba4e4f92af49d322f78186fd6b7281360c
mgltorsa <mgltorsa@gmail.com>
2018-11-24 17:11:02 -0500

    se creó el folder views para fxmls

--- src/controladora/Controladora.java
+++ src/controladora/Controladora.java
@@ -1,18 +1,30 @@
 package controladora;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Controladora extends Application{
 
+	
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader f = new FXMLLoader();
 		
+		FileInputStream file = new FileInputStream(new File(""));
+		Pane pane = f.load(file);
+		
+		//INICIALIZAR PRINCIPAL
 		
 	}
+	
+	
 
 	
 }
