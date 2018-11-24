6db5f4f32c39632a49d7b10020431a6e5be8d3db
NeoJeison <jeyson7@outlook.com>
2018-11-24 15:35:15 -0500

    Espacio en ventana

---
+++ src/vista/Main.java
@@ -0,0 +1,31 @@
+package vista;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("ViewConsultas.fxml"));
+			Parent root = (Parent)loader.load();
+//			VentanaPrincipalVisual vent = (VentanaPrincipalVisual)loader.getController();
+//			Controlador cont = new Controlador(vent);
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
