780677c0b4b33d5bf9af2718675c062446d730e2
mgltorsa <mgltorsa@gmail.com>
2018-11-25 12:34:13 -0500

    cambios en fmxls

---
+++ src/vista/ViewRegistrarSolicitud.java
@@ -0,0 +1,63 @@
+package vista;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javafx.fxml.*;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+public class ViewRegistrarSolicitud {
+
+	private Pane paneContenido;
+
+	@FXML
+	private VBox vBoxPanel;
+
+	@FXML
+	private ChoiceBox<?> chbTipoSolicitud;
+
+	@FXML
+	private ImageView imgBanner;
+
+	@FXML
+	private Button bRegistrar;
+
+	/**
+	 * Tambien inicializa el banner en con la imagen views/imagenes/banner.jpg
+	 * @param paneContenido panel root de la vista
+	 */
+	
+	public void inicializar(Pane paneContenido) {
+		this.paneContenido = paneContenido;
+		modificarBanner("views/imagenes/banner.jpg");
+	}
+
+	public Pane obtenerPaneContenido() {
+		return paneContenido;
+	}
+
+	public Pane obtenerVBoxPane() {
+		return vBoxPanel;
+	}
+
+	public Button obtenerBotonRegistrar() {
+		return bRegistrar;
+	}
+
+	public void modificarBanner(String rutaImagen) {
+		FileInputStream streamImagen;
+		try {
+			streamImagen = new FileInputStream(rutaImagen);
+			imgBanner.setImage(new Image(streamImagen, imgBanner.getFitWidth(), imgBanner.getFitHeight(), false, false));
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
