098f3073ef547ddd94edc3ecc405538fe65db118
NeoJeison <jeyson7@outlook.com>
2018-11-25 10:40:08 -0500

    Arreglo en controladora

--- src/controladora/Controladora.java
+++ src/controladora/Controladora.java
@@ -9,7 +9,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import vista.ViewInicioSesion;
 
 public class Controladora extends Application{
 
