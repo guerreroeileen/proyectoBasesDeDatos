89adce8d9b2760f4470eb2fa0feda7eccdb75ca6
alejandroMu <alejandrobravo0329@gmail.com>
2018-11-25 16:54:11 -0500

    compilo correctamente pk n3 de asignacion y atencion

--- Script Nivel 3/pkAsignacionNivel3.sql
+++ Script Nivel 3/pkAsignacionNivel3.sql
@@ -1,41 +1,39 @@
-CREATE OR REPLACE PACKAGE pkAsignacionNivel3 IS
+CREATE OR REPLACE PACKAGE pkasignacionnivel3 IS
+    FUNCTION fasignacionautomatica (
+        ividsolicitud IN VARCHAR2
+    ) RETURN VARCHAR2;
 
-FUNCTION Fasignacionautomatica (ividsolicitud IN VARCHAR2)RETURN VARCHAR2;
-FUNCTION FAsignacionIndividual (ividsolicitud IN VARCHAR2, ividFuncionario in varchar2)RETURN VARCHAR2;
+    FUNCTION fasignacionindividual (
+        ividsolicitud     IN VARCHAR2,
+        ividfuncionario   IN VARCHAR2
+    ) RETURN VARCHAR2;
 
-end pkAsignacionNivel3;
-
+END pkasignacionnivel3;
 /
-CREATE OR REPLACE PACKAGE BODY pkClienteNivel3 IS
 
-FUNCTION fasignacionautomatica(ividsolicitud IN VARCHAR2)
-return varchar2
-is
-begin
-pkAsignacionNivel2.ppasignacionautomatica(ividsolicitud );
-RETURN 'No_Exception';
+CREATE OR REPLACE PACKAGE BODY pkasignacionnivel3 IS
 
-EXCEPTION
+    FUNCTION fasignacionautomatica (
+        ividsolicitud IN VARCHAR2
+    ) RETURN VARCHAR2 IS
+    BEGIN
+        pkasigancionnivel2.pasignacionautomatica(ividsolicitud);
+        RETURN 'No_Exception';
+    EXCEPTION
+        WHEN OTHERS THEN
+            RETURN sqlerrm;
+    END fasignacionautomatica;
 
-WHEN OTHERS THEN
-RETURN SQLERRM;
+    FUNCTION fasignacionindividual (
+        ividsolicitud     IN VARCHAR2,
+        ividfuncionario   IN VARCHAR2
+    ) RETURN VARCHAR2 IS
+    BEGIN
+        pkasigancionnivel2.pasignacionindividual(ividsolicitud,ividfuncionario);
+        RETURN 'No_Exception';
+    EXCEPTION
+        WHEN OTHERS THEN
+            RETURN sqlerrm;
+    END fasignacionindividual;
 
-end fasignacionautomatica;
-
-
-FUNCTION fasignacionautomatica(ividsolicitud IN VARCHAR2, ividFuncionario in varchar2)
-return varchar2
-is
-begin
-pkAsignacionNivel2.pAsignacionIndividual(ividsolicitud, ividFuncionario );
-RETURN 'No_Exception';
-
-EXCEPTION
-
-WHEN OTHERS THEN
-RETURN SQLERRM;
-
-end fasignacionautomatica;
-
-
-end pkAsignacionNivel3;
+END pkasignacionnivel3;
