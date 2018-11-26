bef09924d30324f2e85a07bf8fb0dff2eabbc815
NeoJeison <jeyson7@outlook.com>
2018-11-25 20:08:30 -0500

    view asignar creada

---
+++ src/vista/ViewAsignarSolicitud.java
@@ -0,0 +1,63 @@
+package vista;
+
+import java.util.HashMap;
+
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class ViewAsignarSolicitud {
+
+    @FXML
+    private Label labCodSolicitud;
+
+    @FXML
+    private Label labCedulaFuncionario;
+
+    @FXML
+    private TextField txtCodSolicitud;
+
+    @FXML
+    private TextField txtCedulaFuncionario;
+
+    @FXML
+    private Button butAsignarSolicitud;
+    
+    private Stage stage;
+    
+    public void inicializar(String name, Pane paneContenido) {
+		stage = new Stage();
+		stage.setScene(new Scene(paneContenido));
+		stage.setTitle(name);
+		stage.setResizable(false);
+		
+	}
+
+	public TextField getTxtCodSolicitud() {
+		return txtCodSolicitud;
+	}
+
+	public void setTxtCodSolicitud(TextField txtCodSolicitud) {
+		this.txtCodSolicitud = txtCodSolicitud;
+	}
+
+	public TextField getTxtCedulaFuncionario() {
+		return txtCedulaFuncionario;
+	}
+
+	public void setTxtCedulaFuncionario(TextField txtCedulaFuncionario) {
+		this.txtCedulaFuncionario = txtCedulaFuncionario;
+	}
+
+	public Button getButAsignarSolicitud() {
+		return butAsignarSolicitud;
+	}
+    
+    
+
+}
+
